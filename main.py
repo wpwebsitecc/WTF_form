811e9141fe8b3e6ff50685f24fd4630801913ea2
wpwebsitecc <wpwebsitecc@gmail.com>
2024-01-17 12:27:49 +0800

    Add Data Validation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField
-
-
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+import email_validator
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -15,12 +15,17 @@ pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
 '''
+
+
 class MyForm(FlaskForm):
-    email = StringField('Email')
-    password = StringField('Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email()]) #DataRequired makes the two fields required fields
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Submit')
+
 
 app = Flask(__name__)
 app.secret_key = "thisisascretkey"
+
 
 @app.route("/")
 def home():
@@ -30,6 +35,7 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = MyForm()
+    form.validate_on_submit()
     return render_template('login.html', form=form)
 
 
